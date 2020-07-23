8260e1ecde228eb63a1924fb71f1a1de717bf159
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2020-07-23 12:17:29 -0400

    fix env var in win.bat

--- .ci/win.bat
+++ .ci/win.bat
@@ -1,7 +1,7 @@
 @echo on
 :: this is a nasty hack, and should not be needed, but the env isn't _quite_ right
 
-set WIN_CI=true
+set SKIP_PREFLIGHT=true
 
 call deactivate
 call C:\Miniconda\envs\ipyradiant-base\Scripts\activate
