8d09b4ceac09485baed62a93765d512dc2137e5a
Nijhawan, Lukas <Lukas.Nijhawan@gtri.gatech.edu>
2020-08-20 15:38:21 -0400

    ran linter

--- src/ipyradiant/__init__.py
+++ src/ipyradiant/__init__.py
@@ -4,10 +4,10 @@
 # Distributed under the terms of the Modified BSD License.
 
 from ._version import __version__
+from .basic_tools import MultiPanelSelect
 from .loader import FileManager, PathLoader, UpLoader
 from .query import QueryWidget
 from .visualization import CytoscapeVisualizer, DatashaderVisualizer, LayoutSelector
-from .basic_tools import MultiPanelSelect
 
 __all__ = [
     "__version__",
