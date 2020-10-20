0733d9d04c504e9b615a838971405109b93fe902
Nijhawan, Lukas <Lukas.Nijhawan@gtri.gatech.edu>
2020-10-20 14:51:03 -0400

    ran linter

--- examples/tests/test_CustomURI.py
+++ examples/tests/test_CustomURI.py
@@ -1,41 +1,53 @@
-import pytest
 import unittest
-from ipyradiant import MultiPanelSelect, PathLoader, FileManager, ObjectLiteralApp, CustomURIRef
-from rdflib import URIRef, Graph
+
+from ipyradiant import CustomURIRef
+from rdflib import Graph, URIRef
+
 
 class Testing(unittest.TestCase):
-    namespaces = {'ex':'www.example.org/'}
+    namespaces = {"ex": "www.example.org/"}
     graph = Graph()
 
     def __init__(self, *args, **kwargs):
         super(Testing, self).__init__(*args, **kwargs)
         for key, value in self.namespaces.items():
             self.graph.namespace_manager.bind(key, value)
-   
+
     # start by testing functionality of CustomURIRef class
     def test1(self):
-        custom = CustomURIRef(uri=URIRef('www.example.org/1'), namespaces = self.namespaces)
-        normal = URIRef('www.example.org/1')
+        custom = CustomURIRef(
+            uri=URIRef("www.example.org/1"), namespaces=self.namespaces
+        )
+        normal = URIRef("www.example.org/1")
         self.assertEqual(custom, normal)
-        
+
     def test2(self):
-        custom = str(CustomURIRef(uri=URIRef('www.example.org/1'), namespaces = self.namespaces))
-        normal = 'ex:1'
+        custom = str(
+            CustomURIRef(uri=URIRef("www.example.org/1"), namespaces=self.namespaces)
+        )
+        normal = "ex:1"
         self.assertEqual(custom, normal)
-        
-    def test3(self):        
+
+    def test3(self):
         # now do similar stuff but with a NamespaceManager object instead...
-        custom = CustomURIRef(uri = URIRef('www.example.org/1'), namespaces = self.graph.namespace_manager)
-        normal = URIRef('www.example.org/1')
+        custom = CustomURIRef(
+            uri=URIRef("www.example.org/1"), namespaces=self.graph.namespace_manager
+        )
+        normal = URIRef("www.example.org/1")
         self.assertEqual(custom, normal)
-        
+
     def test4(self):
-        custom = str(CustomURIRef(uri = URIRef('www.example.org/1'), namespaces = self.graph.namespace_manager))
-        normal = 'ex:1'
+        custom = str(
+            CustomURIRef(
+                uri=URIRef("www.example.org/1"), namespaces=self.graph.namespace_manager
+            )
+        )
+        normal = "ex:1"
         self.assertEqual(custom, normal)
-        
+
     def test5(self):
-        custom = CustomURIRef(uri=URIRef('www.example.org/1'), namespaces = self.namespaces)
-        normal = URIRef('www.example.org/2')
-        self.assertNotEqual(custom,normal)
-        
+        custom = CustomURIRef(
+            uri=URIRef("www.example.org/1"), namespaces=self.namespaces
+        )
+        normal = URIRef("www.example.org/2")
+        self.assertNotEqual(custom, normal)
