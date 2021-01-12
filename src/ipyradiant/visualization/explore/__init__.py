45538f7de692d184d0f4638912cb93343cc73e57
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-01-12 10:12:16 -0500

    Copyright updates and all init import

--- src/ipyradiant/visualization/explore/__init__.py
+++ src/ipyradiant/visualization/explore/__init__.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2021 ipyradiant contributors.
+# Distributed under the terms of the Modified BSD License.
+
+__all__ = [
+    "GraphExploreNodeSelection",
+    "RDFSubjectSelectMultiple",
+    "RDFTypeSelectMultiple",
+]
+
 from .graph_explorer import (
     GraphExploreNodeSelection,
     RDFSubjectSelectMultiple,
