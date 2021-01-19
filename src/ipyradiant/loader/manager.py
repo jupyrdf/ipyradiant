db6d165132b0663051e734d8dd9b8d1831ce2af6
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-01-19 10:44:50 -0500

    Update copyright

--- src/ipyradiant/loader/manager.py
+++ src/ipyradiant/loader/manager.py
@@ -1,6 +1,6 @@
 """ loading widgets
 """
-# Copyright (c) 2020 ipyradiant contributors.
+# Copyright (c) 2021 ipyradiant contributors.
 # Distributed under the terms of the Modified BSD License.
 
 import traitlets as T
