ed90e2503369cd0205229e233b791dcb8bc781d0
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-01-28 13:14:27 -0500

    (WIP) Rework additive query widget. Update FileManager stats (now msg).

--- src/ipyradiant/query/__init__.py
+++ src/ipyradiant/query/__init__.py
@@ -3,7 +3,8 @@
 # Copyright (c) 2021 ipyradiant contributors.
 # Distributed under the terms of the Modified BSD License.
 
-__all__ = ["QueryWidget", "service_patch_rdflib"]
+__all__ = ["BasicQueryWidget", "QueryWidget", "service_patch_rdflib"]
 
+from .app import QueryWidget as BasicQueryWidget
 from .query_widget import QueryWidget
 from .utils import service_patch_rdflib
