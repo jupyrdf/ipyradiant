e7157bca5c4f689db30fcc94275e25d601804c4f
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-02-04 13:19:10 -0500

    Changes to CytoscapeViewer

---
+++ src/ipyradiant/visualization/cytoscape/style.py
@@ -0,0 +1,58 @@
+NODE = {
+    "selector": "node",
+    "style": {
+        "color": "black",
+        "background-color": "CadetBlue",
+    },
+}
+
+LABELLED_NODE = {
+    "selector": "node",
+    "style": {
+        "color": "black",
+        "background-color": "CadetBlue",
+        "label": "data(_label)",
+    },
+}
+
+EDGE = {
+    "selector": "edge",
+    "style": {
+        "line-color": "grey",
+        "opacity": "0.6",
+    },
+}
+
+LABELLED_EDGE = {
+    "selector": "edge",
+    "style": {
+        "font-size": "12",
+        "font-style": "italic",
+        "line-color": "grey",
+        "line-opacity": "0.6",
+        "label": "data(_label)",
+    },
+}
+
+
+DIRECTED_EDGE = {
+    "selector": "edge.directed",
+    "style": {
+        "curve-style": "bezier",
+        "target-arrow-shape": "triangle"
+    },
+}
+
+MULTIPLE_EDGES = {
+    "selector": "edge.multiple_edges",
+    "style": {
+        "curve-style": "bezier"
+    },
+}
+
+DIRECTED_GRAPH = [
+    LABELLED_NODE,
+    LABELLED_EDGE,
+    DIRECTED_EDGE,
+    MULTIPLE_EDGES,
+]
--- src/ipyradiant/visualization/improved_cytoscape.py
+++ src/ipyradiant/visualization/improved_cytoscape.py
@@ -5,75 +5,83 @@ import ipywidgets as W
 import networkx as nx
 import rdflib
 import traitlets as T
+
+from ipycytoscape.cytoscape import Graph as CytoscapeGraph
 from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
 
-STYLE = [
-    {
-        "selector": "node",
-        "css": {
-            "color": "black",
-            "background-color": "CadetBlue",
-        },
-    },
-    {
-        "selector": "edge.directed",
-        "style": {
-            "curve-style": "bezier",
-            "target-arrow-shape": "triangle",
-            "line-color": "grey",
-        },
-    },
-    {"selector": "edge.multiple_edges", "style": {"curve-style": "bezier"}},
-]
+from ipyradiant.visualization.cytoscape import style
+from ipyradiant.rdf2nx import RDF2NX
 
 
 class CytoscapeViewer(W.VBox):
+    """TODO some docs explaining the attrs"""
+
+    animate = T.Bool(default_value=True)
     layouts = T.List()
     layout_selector = T.Instance(W.Dropdown)
     cytoscape_widget = T.Instance(cyto.CytoscapeWidget)
-    data = T.Union(
+    graph = T.Union(
         (
             T.Instance(rdflib.Graph),
-            T.Instance(nx.MultiDiGraph),
-            T.Instance(nx.DiGraph),
             T.Instance(nx.Graph),
-        )
+        ),
+        allow_none=True,
+        default_value=None,
     )
-    cyto_layout = T.Unicode()
+    cyto_layout = T.Unicode(default_value="random")
     cyto_style = T.List()
+    # Can be overwritten prior to assigning `graph` trait
+    _rdf_label = "rdfs:label"
+    _nx_label = "label"
+    # Can be overwritten to change RDF->LPG behavior
+    _rdf_converter: RDF2NX = RDF2NX
 
     @T.default("cyto_style")
     def _make_cyto_style(self):
-        return STYLE
+        return style.DIRECTED_GRAPH
 
-    @T.default("cyto_layout")
-    def _make_default_layout(self):
-        return "cola"
+    # TODO validate graph and throw warning if # nodes is large?
 
-    @T.default("data")
-    def _make_default_graph(self):
-        return None
-
-    @T.observe("data")
-    def _update_data(self, change):
-        # TODO: Clear Graph so that it isn't duplicated
-        if isinstance(self.data, nx.MultiDiGraph) or isinstance(self.data, nx.Graph):
-            self.cytoscape_widget.graph.add_graph_from_networkx(self.data)
-        if isinstance(self.data, rdflib.Graph):
-            nx_graph = rdflib_to_networkx_multidigraph(self.data)
+    @T.observe("graph")
+    def _update_graph(self, change):
+        # Clear graph so that data isn't duplicated
+        self.cytoscape_widget.graph = CytoscapeGraph()
+        if isinstance(self.graph, nx.Graph):
+            self.cytoscape_widget.graph.add_graph_from_networkx(self.graph)
+            # TODO def add_label_from_nx
+            for node in self.cytoscape_widget.graph.nodes:
+                node.data["_label"] = node.data.get(self._nx_label, None)
+        elif isinstance(self.graph, rdflib.Graph):
+            # Note: rdflib_to_networkx_multidigraph does not store the predicate AT ALL, 
+            #  so it is basically unrecoverable (e.g. for labelling); using _rdf_converter
+            nx_graph = self._rdf_converter.convert(self.graph)
             self.cytoscape_widget.graph.add_graph_from_networkx(nx_graph)
+            for node in self.cytoscape_widget.graph.nodes:
+                node.data["_label"] = node.data.get(self._rdf_label, node.data.get("id", None))
 
     @T.default("cytoscape_widget")
     def _make_cytoscape_widget(self):
-        return cyto.CytoscapeWidget()
+        widget = cyto.CytoscapeWidget()
+        widget.set_layout(
+            animate=self.animate,
+            randomize=True,
+            # Fast animation time for initial graph
+            maxSimulationTime=1000,
+        )
+        widget.set_style(self.cyto_style)
+        return widget
 
     @T.default("layouts")
     def _make_layouts(self):
-        return ["cola", "concentric", "grid", "circle"]
+        # TODO increase spacing on the layout?
+        # https://stackoverflow.com/questions/54015729
+        return ["circle", "cola", "concentric", "cose", "dagre", "grid", "random"]
 
     @T.default("layout_selector")
     def _make_layout_selector(self):
-        return W.Dropdown(options=self.layouts)
+        widget = W.Dropdown(description="Layout:", options=self.layouts)
+        T.link((self, "cyto_layout"), (widget, "value"))
+        return widget
 
     @T.observe("layouts")
     def _update_layout_selector(self, change):
@@ -81,7 +89,14 @@ class CytoscapeViewer(W.VBox):
 
     @T.observe("cyto_layout")
     def _update_layout(self, change):
-        self.cytoscape_widget.set_layout(name=self.cyto_layout)
+        # TODO move to .cytoscape.layout
+        # note: animate=False goes through the full simulation to compute layout, which is SLOW
+        self.cytoscape_widget.set_layout(
+            name=self.cyto_layout,
+            animate=self.animate,
+            randomize=True,
+            maxSimulationTime=2000,
+        )
 
     @T.observe("cyto_style")
     def _update_style(self, change):
@@ -100,8 +115,3 @@ class CytoscapeViewer(W.VBox):
                 self.cytoscape_widget,
             )
         return children
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        T.link((self.layout_selector, "value"), (self, "cyto_layout"))
-        self.cytoscape_widget.set_style(self.cyto_style)
