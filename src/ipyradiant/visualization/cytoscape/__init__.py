d3073ef038561f9f186256a9ed4b69dcd08d8aac
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-03-02 09:55:24 -0500

    Lint. Add copyright, etc.

--- src/ipyradiant/visualization/cytoscape/__init__.py
+++ src/ipyradiant/visualization/cytoscape/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 ipyradiant contributors.
+# Distributed under the terms of the Modified BSD License.
+
 __all__ = ["CytoscapeVisualizer"]
 
 from .app import CytoscapeVisualizer
--- src/ipyradiant/visualization/cytoscape/style.py
+++ src/ipyradiant/visualization/cytoscape/style.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 ipyradiant contributors.
+# Distributed under the terms of the Modified BSD License.
+
 NODE = {
     "selector": "node",
     "style": {
