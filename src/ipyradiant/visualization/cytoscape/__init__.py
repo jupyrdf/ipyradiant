94eba7c3a5bf40b39ec80f5c7f47b84128be3b7f
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-04-30 11:04:37 -0400

    Add ConjunctiveGraphViewer widget.

--- src/ipyradiant/visualization/cytoscape/__init__.py
+++ src/ipyradiant/visualization/cytoscape/__init__.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2021 ipyradiant contributors.
 # Distributed under the terms of the Modified BSD License.
 
-__all__ = ["CytoscapeViewer", "CytoscapeVisualizer", "InteractiveViewer"]
+__all__ = [
+    "ConjunctiveGraphViewer",
+    "CytoscapeViewer",
+    "CytoscapeVisualizer",
+    "InteractiveViewer",
+]
 
 from .app import CytoscapeVisualizer
+from .conjunctive import ConjunctiveGraphViewer
 from .interactive import InteractiveViewer
 from .viewer import CytoscapeViewer
