0c9ff63fc2ca7c2bb372ca416b4a38ec53a08ee9
Welz, Zach <Zach.Welz@gtri.gatech.edu>
2021-06-25 13:35:19 -0400

    GH-116: Remove ConjunctiveGraphViewer.

--- src/ipyradiant/visualization/cytoscape/__init__.py
+++ src/ipyradiant/visualization/cytoscape/__init__.py
@@ -2,13 +2,11 @@
 # Distributed under the terms of the Modified BSD License.
 
 __all__ = [
-    "ConjunctiveGraphViewer",
     "CytoscapeViewer",
     "CytoscapeVisualizer",
     "InteractiveViewer",
 ]
 
 from .app import CytoscapeVisualizer
-from .conjunctive import ConjunctiveGraphViewer
 from .interactive import InteractiveViewer
 from .viewer import CytoscapeViewer
