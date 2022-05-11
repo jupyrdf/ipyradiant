d47e60cb6ce792587d9c33a01f16fbc7c8044aa3
Welz <Zach.Welz@gtri.gatech.edu>
2022-05-11 10:01:13 -0400

    GH-125: hack

--- .ci/win.bat
+++ .ci/win.bat
@@ -2,6 +2,7 @@
 :: this is a nasty hack, and should not be needed, but the env isn't _quite_ right
 :: call deactivate
 :: call C:\Miniconda\envs\ipyradiant-base\Scripts\activate
+call pip install git+https://github.com/jvesely/cloudpickle.git@master
 call doit -n4 %%*
 call doit %%* || goto :error
 
